fca305720254bdf4f1d279dad0481386beed254e
fabi123456a <51160913+fabi123456a@users.noreply.github.com>
2023-07-17 10:54:53 +0200

    e213e

--- pages/threejs/Main.tsx
+++ pages/threejs/Main.tsx
@@ -479,6 +479,7 @@ export default function Main(props: {
         alert("licht");
         return;
       }
+
       model = convertTypeObjectPropsToModel(
         objProp,
         props.scene.id,
